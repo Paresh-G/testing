3cdf4266325f84bfdbb9bc13a27a05c1f9f8874f
Paresh-G <pareshgoenka8@gmail.com>
2026-04-08 01:28:21 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Paresh goenka-2222-5!" );
+        System.out.println( "Hello Paresh goenka-2!" );
     }
 }
